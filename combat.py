ef4afeb031691036a2b1ae2ee3b3b6dd4db18920
PatrickDattilio <patrick.dattilio@gmail.com>
2015-10-14 20:53:23 -0400

    Initial commit

---
+++ combat.py
@@ -0,0 +1,119 @@
+import datetime
+import os
+import random
+import time
+import json
+import re
+from action import Action
+
+
+class Combat:
+    def __init__(self, hal_print, add_action, remove_action, queue, free, action):
+        with open(os.path.dirname(__file__) + '/settings.json', 'r') as f:
+            self.settings = json.load(f)
+
+        self.rotation = self.settings['rotation']
+        self.weapon = self.settings['weapon']
+        self.retreat = False
+        self.hal_print = hal_print
+        self.add_action = add_action
+        self.remove_action = remove_action
+        self.queue = queue
+        self.free = free
+        self.action = action
+        self.rollPattern = re.compile('Success: (\d+), Roll: (\d+)')
+        self.killPattern = re.compile('You slit (.*)\'s')
+
+    def set_send_command(self, send_command):
+        self.send_command = send_command
+
+    def set_echo(self, echo):
+        self.echo = echo
+
+    def recover(self):
+        self.send_cmd("get " + self.weapon)
+        time.sleep(random.randrange(1234, 2512) / 1000)
+        self.send_cmd("wie " + self.weapon)
+        self.free = True
+        time.sleep(random.randrange(1593, 2849) / 1000)
+        self.perform_action()
+
+    def handle_recover(self, recover_now):
+        self.add_action(Action.recover)
+        if recover_now:
+            self.perform_action()
+
+    def attack(self):
+        index = random.randrange(0, len(self.rotation))
+        cmd = self.rotation[index]
+        self.send_cmd(cmd)
+        self.add_action(Action.attack)
+
+    # def retreat(self, is_retreating):
+    #     retreat = is_retreating
+    #     pass
+
+    def perform_action(self):
+        if self.free and len(self.queue) > 0:
+            self.action = self.queue.pop()
+            if self.action == Action.recover:
+                self.recover()
+            elif self.action == Action.retreat:
+                self.free = False
+            elif self.action == Action.kill:
+                self.free = False
+                self.add_action(Action.kill)
+                self.send_cmd("kl")
+            elif self.action == Action.attack:
+                self.free = False
+                self.attack()
+            elif self.action == Action.release:
+                self.send_cmd("release")
+            else:
+                self.perform_action()
+
+    # We are in combat
+    def handle_combat_line(self, line):
+        me = True
+        if "You are no longer busy." in line:
+            self.free = True
+            self.perform_action()
+        elif "expires." in line:
+            self.remove_action(Action.kill)
+            self.hal_print("Dead")
+            self.in_combat = False
+        elif "falls unconscious" in line:
+            self.remove_action(Action.attack)
+            self.add_action(Action.kill)
+            if self.free:
+                self.perform_action()
+        elif "You fumble!" in line:
+            self.handle_recover(False)
+        elif "You must be wielding a weapon to attack." in line or "You can't do that right now." in line:
+            self.handle_recover(True)
+        elif "clamped onto you" in line:
+            self.add_action(Action.release)
+        elif "You manage to break free!" in line:
+            self.remove_action(Action.release)
+        elif "must be unconscious first" in line:
+            self.remove_action(Action.kill)
+            self.free = True
+        elif "[" in line and "Success" in line:
+            if "] A" in line or "] An" in line:
+                self.add_action(Action.attack)
+                me = False
+                if self.free:
+                    self.hal_print("Free, attacking")
+                    self.perform_action()
+            elif "You slit" in line:
+                target = self.killPattern.search(line)
+                if target:
+                    self.hal_print(str(datetime.datetime.now())[:-7] + " Killed " + target.group(1))
+                self.remove_action(Action.kill)
+                self.in_combat = False
+            roll = self.rollPattern.search(line)
+            if me:
+                self.action_status = int(roll.group(1)) < int(roll.group(2))
+
+    def send_cmd(self, cmd):
+        self.send_command(cmd + "\n")
---
+++ scoundrel.py
@@ -0,0 +1,82 @@
+import tkinter as tk
+from action import Action
+from combat import Combat
+
+
+class Plugin:
+    def __init__(self):
+        print("Hal: init")
+        self.in_combat = False
+        self.free = True
+        self.action = Action.nothing
+        self.queue = []
+        self.combat = Combat(self.hal_print, self.add_action, self.remove_action, self.queue, self.free,
+                             self.action)
+        self.combat_enabled = tk.BooleanVar()
+
+    def set_send_command(self, send_command):
+        self.send_command = send_command
+        self.combat.set_send_command(send_command)
+
+    def set_echo(self, echo):
+        self.echo = echo
+        self.combat.set_echo(echo)
+
+    def post_process(self, line):
+        if self.in_combat and self.combat_enabled.get():
+            self.combat.handle_combat_line(line)
+        elif "You are no longer busy" in line:
+            self.free = True
+            # self.perform_action()
+        elif ("] A" in line or "] An" in line) and "You retrieve the line" not in line:
+            self.hal_print("Combat")
+            self.in_combat = True
+        elif "retreat" in line and "You retreat." not in line and "retreat first" not in line and "retreats." not in line:
+            self.hal_print("Retreating")
+            self.add_action(Action.retreat)
+
+    def add_action(self, action):
+        if action not in self.queue:
+            self.queue.append(action)
+            self.queue.sort()
+
+    def remove_action(self, action):
+        if action in self.queue:
+            self.queue.remove(action)
+            self.queue.sort()
+
+    def draw(self, plugin_area):
+        label_frame = tk.LabelFrame(plugin_area, text="Hal")
+        label_frame.grid(row=0, column=0, sticky=tk.N)
+        self.draw_toggles(label_frame)
+        self.draw_text(label_frame)
+
+    def draw_toggles(self, label_frame):
+        combat_toggle = tk.Checkbutton(label_frame, text="Combat", variable=self.combat_enabled,
+                                       command=self.combat.perform_action)
+        combat_toggle.grid(row=0, column=0, sticky=tk.N)
+
+    def draw_text(self, label_frame):
+        scrollbar = tk.Scrollbar(label_frame)
+        scrollbar.grid(row=1, column=1, sticky=tk.N + tk.S)
+        self.hal_output = tk.Text(
+            label_frame,
+            state=tk.DISABLED,
+            name="hal_output",
+            yscrollcommand=scrollbar.set,
+            wrap=tk.WORD
+        )
+        scrollbar.config(command=self.hal_output.yview)
+        self.hal_output.scrollbar = scrollbar
+        self.hal_output.grid(row=1, column=0, sticky=tk.N + tk.W)
+
+    def scroll_output(self):
+        self.hal_output.see(tk.END)
+
+    def hal_print(self, line):
+        self.hal_output.insert(tk.END, line + "\n", None)
+        self.scroll_output()
+
+    def perform_action(self):
+        if self.in_combat and self.combat_enabled.get():
+            self.combat.perform_action()
